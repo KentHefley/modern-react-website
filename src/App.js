6d4877a630f00f8ef5ab3284cbeb1a11639317a7
unknown <hefleykent@gmail.com>
2022-01-20 13:58:40 -0600

    added all files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
-
+import './App.css';
+import {Footer, Blog, Possibility, Features, WhatGPT3, Header} from './containers';
+import {Cta, Brand, Navbar,} from './components';
 
 function App() {
     return (
-        <div>
-        <h1>Modern React Website</h1>
+        <div className='App'>
+            <div className='gradient__bg'>
+                <Navbar/>
+                <Header/>
+            </div>
+                <Brand/>
+                <WhatGPT3/>
+                <Features/>
+                <Possibility/>
+                <Cta/>
+                <Blog/>
+                <Footer/>  
         </div>
     )
 }
